6fc207b3cc182d05758535fbc30c27363ccc563b
anas ismail <msanasismail@gmail.com>
2024-06-12 12:00:53 +0530

    12 june

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,12 @@
-export async function GET() {
+import { headers } from "next/headers"
+import { type NextRequest } from "next/server"
+
+export async function GET(request : NextRequest) {
+    const requestHeaders = new Headers(request.headers)
+    const headerList = headers()
+
+    console.log(requestHeaders.get("Authorization"))
+    console.log(requestHeaders.get("Authorization"))
+    
     return new Response("Profile API Data")
 }
