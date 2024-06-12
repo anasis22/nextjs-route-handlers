ef45af58f3ca85a063494ff58f9d354978640d40
anas ismail <msanasismail@gmail.com>
2024-06-12 16:38:58 +0530

    12 june

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,12 +1,21 @@
-import { headers } from "next/headers"
+import { cookies, headers } from "next/headers"
 import { type NextRequest } from "next/server"
 
 export async function GET(request : NextRequest) {
     const requestHeaders = new Headers(request.headers)
     const headerList = headers()
+    const theme = request.cookies.get("theme")
+    cookies().set("pages","3")
 
     console.log(requestHeaders.get("Authorization"))
-    console.log(requestHeaders.get("Authorization"))
+    console.log(headerList.get("Authorization"))
+    console.log(theme?.value)
+    console.log(cookies().get("pages"))
     
-    return new Response("Profile API Data")
+    return new Response("<h1>Profile API Data</h1>",{
+        headers: {
+            "Content-Type" : "text/html",
+            "set-Cookie" : "theme=dark"
+        },
+    })
 }
---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse , type NextRequest } from "next/server";
+
+export function middleware(request : NextRequest){
+
+    const response = NextResponse.next()
+
+    const themePreference = request.cookies.get("theme")
+
+    if(!themePreference){
+        response.cookies.set("theme","light")
+    }
+
+    response.headers.set("custom-header","custom-value")
+    
+    return response;
+   
+
+
+    // if(request.nextUrl.pathname === "/time"){
+    //     return NextResponse.rewrite(new URL("/hello",request.url))
+    // }
+}
